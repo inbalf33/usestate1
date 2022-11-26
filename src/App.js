77fafad68a8c8475b55fdba4def5555a00628439
inbalf33 <inbalf33@gmail.com>
2022-11-26 21:34:32 +0200

    Ex3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 import Counter from './Counter';
+import { useState } from 'react';
 
 function App() {
+
+const [delta, setDelta] = useState(1);
+
+function handleDelta(e){
+  setDelta(Number(e.target.value));
+}
+
+const [maxCounter, setMaxCounter] = useState(1);
+
+function handleMaxCounter(e){
+  setMaxCounter(Number(e.target.value));
+}
+
   return (
     <div className="App">
-      <h2> <u>Inbal Fish Usestate</u></h2>
-      <Counter />
+      <h2> <u>Inbal Fish Usestate</u></h2>      
+      <div>  Number of jumps: <input type = "number" value = {delta} onChange = {handleDelta} /> </div>
+      <div> Maximum number: <input type = "number" value = {maxCounter} onChange = {handleMaxCounter} /> </div>
+      <Counter delta = {delta} maxCounter = {maxCounter}/>
+      <Counter delta = {delta} maxCounter = {maxCounter}/>
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,31 +1,38 @@
 import './Counter.css';
 import { useState } from "react";
 
-function Counter() {
-    const [count, setCount] = useState(0);
+
+function Counter(props) {
+    const {delta} = props;
+    const {maxCounter} = props;
+    const [count, setCount] = useState(0);   
 
     function incr(){
         setCount(
             function(oldCount){
-                return oldCount + 1;
+                if (oldCount + delta > maxCounter)
+                {
+                    return 0;
+                }
+                else {
+                    return oldCount + delta;
+                }
+                
             }
         )
-        console.log(count);
+        console.log(maxCounter);
     }
 
     function reset(){
-        setCount(
-            function(oldCount){
-                return oldCount = 0;
-            }
-        )
+        setCount(0)
     }
+
 
     return (
       <div class = "counter">
-        <h2>Counter</h2>
+        <h2>Counter</h2>        
         <p>Counter is at - <div class="countVarible"> {count} </div> </p>
-        <button onClick={incr}>Click to add <b class="txtOne">1</b> to Counter</button>
+        <button onClick={incr}>Click to add <b class="txtOne"> {delta} </b> to Counter</button>
         <button onClick={reset}>Click to  <b class="txtReset">reset</b> the Counter</button>
       </div>
     );
